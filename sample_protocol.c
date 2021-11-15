d380417a265ec2b9cd9f1b5ac51aeed74877ed12
Gergely Gati <gati.gergely@gmail.com>
2021-11-14 22:32:07 -0800

    slave reimplemented using signal based async io in order to make it more similar to an irq based communication

--- sample_protocol.c
+++ sample_protocol.c
@@ -1,4 +1,5 @@
 // gcc -Wall -o sample_protocol sample_protocol.c -lpthread
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -8,6 +9,7 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <time.h>
+#include <signal.h>
 
 // csv parser library
 // https://github.com/gega/ccsv
@@ -74,7 +76,8 @@ static char *errors[]=
 };
 
 #define BUFSIZE (64)
-#define MAXWAIT_US (10000)
+#define MINWAIT_US (10000)
+#define MAXWAIT_US (100000)
 #define SIZEOF_ARR(arr) (sizeof((arr))/sizeof((arr)[0]))
 #define TS2MS(ts) (((ts)->tv_sec*1000L)+(((ts)->tv_nsec/1000000L)))
 
@@ -137,7 +140,7 @@ static void HAL_UART_Transmit(int fd, char *string)
       // realistic environment where the read on the other side can return in the
       // middle of a message -- just like it can in real life as well just less often
       fsync(fd);
-      usleep(rand()%MAXWAIT_US);
+      usleep(MINWAIT_US+rand()%(MAXWAIT_US-MINWAIT_US));
     }
   }
 }
@@ -145,8 +148,11 @@ static void HAL_UART_Transmit(int fd, char *string)
 ///////////////////
 // slave globals
 #define BACKGROUND_TASK_COUNT (3)
+#define SLOW_MIN_DUR_MS (2000)
+#define SLOW_MAX_DUR_MS (8000)
 static struct task slv_slow_in_progress[BACKGROUND_TASK_COUNT]={0};
 static char slv_buf[BUFSIZE];
+static volatile int slave_sigio=0;
 
 
 static int slave_cmd_vers(struct ccsv *csv, int fout)
@@ -169,7 +175,7 @@ static int slave_cmd_slow(struct ccsv *csv, int fout)
   {
     ret=ERR_OK;
     tsk->progress=1;
-    tsk->duration=rand()%3000;
+    tsk->duration=SLOW_MIN_DUR_MS+rand()%(SLOW_MAX_DUR_MS-SLOW_MIN_DUR_MS);
     tsk->p1=atoi(ccsv_nextfield(csv,NULL));
     tsk->p2=atoi(ccsv_nextfield(csv,NULL));
     tsk->p3=atoi(ccsv_nextfield(csv,NULL));
@@ -240,10 +246,18 @@ static int cmp_protocol(const void *key, const void *proto)
   return(strncmp((const char *)key,((struct protocol *)proto)->command,sizeof(((struct protocol *)0)->command)));
 }
 
+void slave_sigio_handler(int sig, siginfo_t *info, void *ucontext)
+{
+  /* SIGIO on info.si_fd */
+  slave_sigio++;
+}
+
 // slave entity (process/ecu/etc)
 static void *uart_slave(void *ud)
 {
   struct fifos *fs=(struct fifos *)ud;
+  struct sigaction sact={0};
+  struct f_owner_ex fex={0};
   rb_t rbv;
   int err;
   char *msg;
@@ -259,36 +273,62 @@ static void *uart_slave(void *ud)
   };
   struct protocol *cmd;
 
-
   // setting up our fifos
   // for slave f1 is input, f2 is output
   int fin=open(fs->f1,O_RDWR);
   int fout=open(fs->f2,O_RDWR);
-  
-  // fifos are ready, let's start the master
-  pthread_mutex_unlock(&fs->mut);
 
+  // setup signal handler for SIGIO on fin
+  sact.sa_sigaction=slave_sigio_handler;
+  sact.sa_flags=SA_NODEFER|SA_SIGINFO;
+  if(0!=sigaction(SIGUSR1,&sact,NULL)) perror("sigaction SIGUSR1");
+
+  // set async flag to receive signals
+  if(0!=fcntl(fin,F_SETFL,fcntl(fin,F_GETFL)|O_ASYNC|O_NONBLOCK)) perror("fcntl ASYNC");
+
+  // set receiver pid  
+  fex.type=F_OWNER_TID;
+  fex.pid=gettid();
+  if(0!=fcntl(fin,F_SETOWN_EX,&fex)) perror("fcntl SETOWN"); 
+
+  // ask kernel to send signals when fin became readable without blocking
+  if(0!=fcntl(fin,F_SETSIG,SIGUSR1)) perror("fcntl SIGUSR1");
+  
   // mmfl init needed
   rb_init(&rbv,buf,sizeof(buf),fin);
   
+  // setup is ready, let's start the master
+  pthread_mutex_unlock(&fs->mut);
+
   // business logic for slave --------------
   
   for(err=0;err>=0;)
   {
-    HAL_UART_Receive(&rbv,&msg);
-    printf("%s: msg received: '%s'\n",__func__,msg);
-    // ccsv used for csv parsing
-    ccsv_init(&csv,msg);
-    if(NULL!=(cmd=bsearch(ccsv_nextfield(&csv,NULL),proto,SIZEOF_ARR(proto),sizeof(struct protocol),cmp_protocol))) 
+    if(0==slave_sigio) sleep(4);
+    if(0<slave_sigio)
     {
-      // execute protocol command
-      err=cmd->fn(&csv,fout);
+      slave_sigio=0;
+      // we have something to read
+      HAL_UART_Receive(&rbv,&msg);
+      if(NULL!=msg)
+      {
+        printf("%s: msg received: '%s'\n",__func__,msg);
+
+        // ccsv used for csv parsing
+        ccsv_init(&csv,msg);
+        if(NULL!=(cmd=bsearch(ccsv_nextfield(&csv,NULL),proto,SIZEOF_ARR(proto),sizeof(struct protocol),cmp_protocol)))
+        {
+          // execute protocol command
+          err=cmd->fn(&csv,fout);
+        }
+        else
+        {
+          snprintf(buf,sizeof(buf),"%d,%s",ERR_NOCOMMAND,errors[ERR_NOCOMMAND]);
+          HAL_UART_Transmit(fout,buf);
+        }
+      }
     }
-    else
-    {
-      snprintf(buf,sizeof(buf),"%d,%s",ERR_NOCOMMAND,errors[ERR_NOCOMMAND]);
-      HAL_UART_Transmit(fout,buf);
-    }
+    else break; // not expecting other signals
   }
 
   // cleanup
@@ -316,7 +356,8 @@ static void *uart_master(void *ud)
   int task[4];
 
   for(i=0;i<SIZEOF_ARR(task);i++) task[i]=-1;
-  
+
+  // wait for the slave to initialize  
   pthread_mutex_lock(&fs->mut);
 
   // setting up our fifos
@@ -334,7 +375,6 @@ static void *uart_master(void *ud)
   // VERSION
   snprintf(buf,sizeof(buf),"VERS,7");
   HAL_UART_Transmit(fout,buf);
-
   HAL_UART_Receive(&rbv,&msg);
   printf("%s: msg received: '%s'\n",__func__,msg);
 
@@ -442,8 +482,8 @@ int main(void)
   pthread_mutex_lock(&fs.mut);
 
   // start the entities
-  pthread_create(&t1,NULL,uart_slave,&fs);
   pthread_create(&t2,NULL,uart_master,&fs);
+  pthread_create(&t1,NULL,uart_slave,&fs);
   
   // collect the remains
   pthread_join(t1,NULL);
